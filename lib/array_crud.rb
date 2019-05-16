269ad916626a0b9e1357b0de1131ecc3dac4d840
bogomilmihaylov <bogomilmihaylov@live.com>
2019-05-16 19:29:22 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  add_element_to_end_of_array.push("Alienata")
+  [ ].push("Alienata")
 end
 
 def add_element_to_start_of_array(array, element)
